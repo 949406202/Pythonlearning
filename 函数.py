5a204d64d50dc45e55e2563e087742943ba6ff4b
949406202 <949406202@qq.com>
2019-09-30 18:25:49 +0800

    新增一个不定长参数的函数使用方法

--- 函数.py
+++ 函数.py
@@ -9,10 +9,15 @@ def add(age,name):
     return '[%s] 今年 [%d]'%(name,age)
 strs = add(name='zhangsan',age=15)#在传参的时候我们指定我们所想要传递的参数名字
 print(strs)
-#默认值参数
+#默认值参数,定义函数的时候尽量将默认参数写在最后
 def add(name,age=18):
     return '[%s] 今年 [%d]'%(name,age)
 strs = add(name='zhangsan')#在传参的时候如果我们没有指定age的时候，就会使用方法定义的默认值
 strs = add('lisi',20)#在传参的时候如果我们指定age的时候，就会使用调用者传递的参数
 print(strs)
-
+#不定长参数，他能处理比当初定义时更多的参数,可以传任意长的参数
+def add(name,age,*args):
+    print("*args = ",*args,",类型是 = ",type(args))
+    return '[%s] 今年 [%d] 岁！！！'%(name,age)
+strs = add('lisi',20,"足球",'篮球')#在传参的时候我们可以传任意长的参数
+print(strs)
