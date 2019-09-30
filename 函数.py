51dedd30eae9b2e849d97ef5d153154b0a0b15c0
949406202 <949406202@qq.com>
2019-09-30 18:07:59 +0800

    添加了一个默认值的使用方式

--- 函数.py
+++ 函数.py
@@ -7,6 +7,12 @@ print(strs)
 #我们在使用关键字函数调用的时候就可以不必须实参顺序一致
 def add(age,name):
     return '[%s] 今年 [%d]'%(name,age)
-strs = add(name='zhangsan',age=15)
+strs = add(name='zhangsan',age=15)#在传参的时候我们指定我们所想要传递的参数名字
+print(strs)
+#默认值参数
+def add(name,age=18):
+    return '[%s] 今年 [%d]'%(name,age)
+strs = add(name='zhangsan')#在传参的时候如果我们没有指定age的时候，就会使用方法定义的默认值
+strs = add('lisi',20)#在传参的时候如果我们指定age的时候，就会使用调用者传递的参数
 print(strs)
 
